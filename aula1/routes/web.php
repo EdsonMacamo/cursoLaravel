2777f1b09881a0557fab29dd01370d322936d5a8
Edson Macamo <edsonmacamo85@gmail.com>
2022-04-06 12:13:09 +0200

    aula 2 blade no laravel

--- aula1/routes/web.php
+++ aula1/routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $nome = "Edson";
+    $idade = 29;
+    return view('welcome', ['nome' => $nome, 'idade' => $idade, 'profissao' =>"Programador"]);
 });
 Route::get('/contact', function () {
     return view('contact');
